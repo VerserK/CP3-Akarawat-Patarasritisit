64bffb093df7d733efc6c4e1d7d2431dce527894
VerserK <zeenzy@gmail.com>
2022-04-18 11:00:26 +0700

    Add  Collect Individual Points

---
+++ Exercise4_Akarawat_P.py
@@ -0,0 +1,14 @@
+# Foundation English
+FE = 94.3
+# General Business
+GB = 85.7
+# Introduction to Computer Systems
+ICS = 97.9
+# Computer Programming
+CP = 99.9
+
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
